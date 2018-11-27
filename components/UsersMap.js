f9b24178d682df0d0b0599143750b96344969ab1
jehul <jehulee97@g.ucla.edu>
2018-11-27 02:13:42 -0800

    partial routes on map

--- components/UsersMap.js
+++ components/UsersMap.js
@@ -1,24 +1,222 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Polyline } from 'react-native-maps';
 
 
 //Component used to show map
+mapStyle = [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#f5f5f5"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#f5f5f5"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#bdbdbd"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#eeeeee"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#e5e5e5"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#dadada"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.line",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#e5e5e5"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.station",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#eeeeee"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#c9c9c9"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    }
+  ]
 const usersMap = props => {
     let userLocationMarker = null;
     let userDestinationMarker  = null;
+    let route = null;
 
     this.state = {
         markers : [],
+        coords: props.coordinates,
+        destinationLatLong: null,
     };
 
     if (props.userLocation) {
-        userLocationMarker = <MapView.Marker coordinate={props.userLocation} pinColor="teal" />
+        userLocationMarker = <MapView.Marker coordinate={props.userLocation} pinColor="gold" />
     }
 
     if (props.destinationLocation) {
-        userDestinationMarker = <MapView.Marker coordinate={props.destinationLocation} />
+        userDestinationMarker = <MapView.Marker coordinate={props.destinationLocation} />;
+       
     }
+
+   
+   
+    console.log(props.coordinates)
+    //this.setState({
+     //   coords: props.coordinates
+    //});
 
    // if (this.state.markers.length != 0){ //if we have stops to mark
     //    stopMarker = <MapView.Marker 
@@ -29,6 +227,10 @@ const usersMap = props => {
      //   />
    //}
 
+    
+   //{this.state.markers.map(marker =>(
+    //<MapView.Marker coordinate={{longitude: marker.longitude, latitude: marker.latitude}} />
+    //))}
     return (
         <View style={styles.mapContainer}>
             <MapView 
@@ -40,15 +242,24 @@ const usersMap = props => {
                   }}
                 region={props.userLocation}
                 
-                style={styles.map}> 
+                style={styles.map}
+                customMapStyle={mapStyle}
+                > 
                 
+                
+             
+               
+               
+                <Polyline 
+                    coordinates={this.state.coords}
+                            
+                    strokeColor="#FF5E5E"
+                    
+                    strokeWidth={6}
+                />
+
                 {userLocationMarker}
                 {userDestinationMarker}
-                {this.state.markers.map(marker =>(
-                <MapView.Marker coordinate={{longitude: marker.longitude, latitude: marker.latitude}} />
-                ))}
-               
-                
 
             </MapView>
         </View>
