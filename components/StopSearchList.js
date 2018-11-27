60ace8040abca49b0892e432b7ff7151003bc886
q7zh <qiliang@ucla.edu>
2018-11-27 01:58:14 -0800

    Add add or update new search bar functionality

---
+++ components/StopSearchList.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {View, StyleSheet, ScrollView} from 'react-native';
+import {TextInput, TouchableOpacity, Text, Image, Button} from 'react-native';
+
+class StopSearchList extends React.Component {
+    render() {
+        return (
+            this.props.stops.map((val, index) => {
+                return (
+                  <View style={styles.row} key={index}>
+                      <TextInput
+                        style={styles.searchInput}
+                        value={this.props.searchString[index]}
+                        onSubmitEditing={() => this.props.submitFunc(index)}
+                        onChange={(e) => this.props.changeFunc(index, e)}
+                        placeholder='Add a Stop'/>
+                      <TouchableOpacity
+                        style={styles.removeStopButton}
+                        onPress={() => this.props.removeFunc(index)}>
+                        <Text style={styles.removeStopButtonText}>×</Text>
+                      </TouchableOpacity>
+                  </View>
+                )
+            })
+        )
+    }
+}
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+  },
+  searchInput: {
+    padding: 15,
+    fontSize: 17,
+    borderBottomColor: '#9B9B9B',
+    borderBottomWidth: 0.5,
+    textAlign: 'left',
+    flex: 4
+  },
+  removeStopButton: {
+    marginTop: 10,
+    marginLeft: 17,
+    marginRight: -30,
+  },
+  removeStopButtonText: {
+    textAlign:'center',
+    color: 'black',
+    fontSize: 25
+  }
+});
+export default StopSearchList
